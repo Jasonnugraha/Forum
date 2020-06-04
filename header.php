f46672c63e5d2aec1e58ccc58b00dcab92b4f2b1
Hendro Leegit config --global user.name Hendro <hendrolee@Hendros-MacBook-Pro-2.local>
2020-06-04 15:31:27 +1000

    nav layout 2

--- header.php
+++ header.php
@@ -36,14 +36,7 @@
                     <ul class="navbar-nav mr-auto mt-2 mt-lg0">
                         <!-- Navbar content -->
                        <div class="container">
-                       <h4 id="Title">Game Forum</h4>
-                            <div class="row">
-                                <div class="col">
-                                    <li class="navbar navbar-nav active">
-                                        <a class="nav-link active" href="index.php">Home</a> 
-                                    </li>
-                                </div>
-                            </div>
+                       <a href="index.php" id="Title">Game Forum</a>
 
                             <div class="row">
                                 <div class="col">
